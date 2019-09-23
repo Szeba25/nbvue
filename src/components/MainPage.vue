20a2f639fb98c27e42ac4fd6f35ec0f56100e81c
Szeba25 <lustmord20@gmail.com>
2019-09-23 18:42:21 +0200

    Finished main page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -14,6 +14,9 @@
                 <div class="highlight-item highlight-item3"><div class="highlight-item-fadein centered">Történetek</div></div>
             </router-link>
         </div>
+        <div class="main-separator">
+            <hr>
+        </div>
         <div class="updates-title centered">
             <h2>Friss tartalmak</h2>
         </div>
@@ -21,14 +24,14 @@
             <div v-for="(item, index) in updates" v-bind:key="item.id">
                 <div v-if="index % 2 == 0" class="updates-div">
                     <router-link v-bind:to=item.link>
-                        <img class="updates-picture" v-bind:src=item.picture width=320px height=240px>
+                        <img class="updates-picture" v-bind:src=item.picture width=318px height=238px>
                     </router-link>
-                    <div class="updates-description">{{item.description}}</div>
+                    <div class="description-bkg"><div class="updates-description">{{item.description}}</div></div>
                 </div>
                 <div v-else class="updates-div">
-                    <div class="updates-description">{{item.description}}</div>
+                    <div class="description-bkg"><div class="updates-description">{{item.description}}</div></div>
                     <router-link v-bind:to=item.link>
-                        <img class="updates-picture" v-bind:src=item.picture width=320px height=240px>
+                        <img class="updates-picture" v-bind:src=item.picture width=318px height=238px>
                     </router-link>
                 </div>
             </div>
@@ -62,13 +65,13 @@ export default {
 .mainpage-content {
     /* Define a two-row grid */
     display: grid;
-    grid-template-rows: min-content 200px min-content auto;
+    grid-template-rows: min-content 200px min-content min-content auto;
     grid-gap: 0px;
     padding: 0px;
 }
 
 .highlights-title {
-    padding: 15px 0px 15px 0px;
+    padding: 30px 0px 15px 0px;
 }
 
 .highlights {
@@ -95,6 +98,8 @@ export default {
 
     filter: grayscale(75%);
     opacity: 0.8;
+
+    box-shadow: 0px 0px 15px 3px #35261a93;
 }
 
 .highlight-item:hover {
@@ -137,6 +142,10 @@ export default {
     margin: 170px 20px 0px 20px;
 }
 
+.main-separator {
+    margin: 45px 0px 15px 0px;
+}
+
 .updates-title {
     padding: 15px 0px 15px 0px;
 }
@@ -148,13 +157,19 @@ export default {
 .updates-div {
     display: grid;
     grid-template-columns: 320px 320px;
+    grid-gap: 20px;
     align-items: center;
     text-align: center;
+    margin: 0px 0px 10px 0px;
 }
 
 .updates-picture {
     vertical-align: middle;
+    border-style: solid;
+    border-color: #2e1808;
+    border-width: 1px;
     border-radius: 15px;
+    box-shadow: 0px 0px 15px 3px #35261a93;
     filter: grayscale(75%);
     opacity: 0.8;
     transition: 0.3s;
@@ -171,5 +186,12 @@ export default {
     font-size: 24px;
     color: #2e1808;
     margin: 0px 20px;
+}
+
+.description-bkg {
+    width: 320px;
+    height: 240px;
+    display: flex;
+    align-items: center;
 }
 </style>>
