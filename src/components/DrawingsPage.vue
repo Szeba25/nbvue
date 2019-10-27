c1bc836cb5886359caa64e56893e370a43b40882
Szeba25 <lustmord20@gmail.com>
2019-10-27 18:00:46 +0100

    Added year selection

--- src/components/DrawingsPage.vue
+++ src/components/DrawingsPage.vue
@@ -1,8 +1,23 @@
 <template>
-    <div class="drawingspage-content">
-        <h2 class="drawings-title centered">Válassz évet!</h2>
-        <div class="drawings-years">
-            <div class="faded-icon" v-bind:style="{'background-image': 'url(/'+item.picture+')'}" v-for="item in years" v-bind:key="item.id"></div>
+    <div v-if="selectedYear">
+        <div class="drawingspage-content">
+            <h2 class="drawings-title centered">{{selectedYear}}. év</h2>
+            <div class="drawings-pictures">
+                <div><div class="faded-icon"></div><p class="drawings-note">Beautiful picture 1</p></div>
+                <div><div class="faded-icon"></div><p class="drawings-note">Beautiful picture 2</p></div>
+                <div><div class="faded-icon"></div><p class="drawings-note">Beautiful picture 3 with a very very long note!<br>and a break!</p></div>
+                <div><div class="faded-icon"></div><p class="drawings-note">Beautiful picture 4</p></div>
+                <div><div class="faded-icon"></div><p class="drawings-note">Beautiful picture 5</p></div>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        <div class="drawingspage-content">
+            <h2 class="drawings-title centered">Válassz évet!</h2>
+            <div class="drawings-years">
+                <div class="faded-icon" v-bind:style="{'background-image': 'url(/'+item.picture+')'}" v-for="item in years" v-bind:key="item.id" v-on:click="selectYear(item.year)"></div>
+                {{selectedYear}}
+            </div>
         </div>
     </div>
 </template>
@@ -15,16 +30,27 @@ export default {
 
     data() {
         return {
-            years: []
+            years: [],
+            selectedYear: ""
         }
     },
 
     created() {
-        Axios.get('/drawings_page/data.json').then((response) => {
+        Axios.get('/drawings_page/years.json').then((response) => {
             for (let i = 0; i < response.data.length; i++) {
                 this.years.push(response.data[i]);
             }
         });
+    },
+
+    methods: {
+        resetYear() {
+            this.selectYear("");
+        },
+
+        selectYear(year) {
+            this.selectedYear = year;
+        }
     }
 };
 </script>
@@ -46,5 +72,19 @@ export default {
     grid-template-columns: 220px 220px 220px;
     grid-gap: 20px;
     margin: 0px auto 30px auto;
+}
+
+.drawings-pictures {
+    display: grid;
+    grid-template-columns: 220px 220px;
+    grid-auto-rows: 320px;
+    grid-row-gap: 10px;
+    grid-column-gap: 50px;
+    margin: 0px auto 80px auto;
+}
+
+.drawings-note {
+    text-align: center;
+    margin: 10px 30px 10px 30px;
 }
 </style>
